5aea427c892afb30050a9e283e2c15115e555eab
project-servises <69837500+project-servises@users.noreply.github.com>
2020-08-18 09:05:56 +0300

    Waxom

---
+++ js/jeson.js
@@ -0,0 +1,47 @@
+$(document).ready(function(){
+    /*$('.header__burger').on('click', function(){
+        $('.header__burger,.header__menu').toggleClass('active');
+    }); 
+
+    $('.services__item').mouseover(function(){
+        $('.services__item a').css({color:'white', transition:'all 2s ease 0s', border:'2px solid white'});     
+    });
+
+    $('.services__item').mouseout(function(){
+        $(".services__item a").css({color:'black',  transition:'all 1s ease 0s', border:'2px solid black'});
+    });
+    */
+
+});
+
+
+
+
+
+
+/*$(document).ready(function(){
+    $('.slider').slick({
+        arrow:true,
+        dots:true,
+        adaptiveHeight:true,
+        slidesToShow:1,
+        slidesToScroll:1,
+        speed: 1000,
+        easing:'ease',
+        infinite:true,
+        initialSlide:0,
+        autoplay:false,
+        waitForAnimate:false,
+        centerMode:false,
+        variableWidth:false,
+        rows:1,
+        slidesPerRow:1,
+        vertical:false,
+        verticalSwiping:false,
+        
+    });
+});
+*/
+
+
+
