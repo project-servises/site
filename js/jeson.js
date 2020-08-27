2f2443ebc2fda9d8684008d6d1672670df19e335
project-servises <69837500+project-servises@users.noreply.github.com>
2020-08-27 04:22:38 +0300

    Update jeson.js

--- js/jeson.js
+++ js/jeson.js
@@ -2,6 +2,27 @@ $(document).ready(function(){
     $('.header__burger').on('click', function(){
         $('.header__burger,.header__menu').toggleClass('active');
     });
+    
+    $('.team__item:first-child').mousemove(function(){
+			$('.team__button ul li:first-child').css({'background': '#3949ab'});
+			$('.team__item:first-child').mouseout(function(){
+				$('.team__button ul li:first-child').css({'background': '#00897b'});
+			});
+		});
+
+		$('.team__item:nth-child(2)').mousemove(function(){
+			$('.team__button ul li:nth-child(2)').css({'background': '#3949ab'});
+			$('.team__item:nth-child(2)').mouseout(function(){
+				$('.team__button ul li:nth-child(2)').css({'background': '#00897b'});
+			});
+		});
+
+		$('.team__item:last-child').mousemove(function(){
+			$('.team__button ul li:last-child').css({'background': '#3949ab'});
+			$('.team__item:last-child').mouseout(function(){
+				$('.team__button ul li:last-child').css({'background': '#00897b'});
+			});
+		});
 
 });
 
